1cf9ea6431c65ce2b88a7daddbc5e8913392c1e2
Conner Swenberg <conner.swenberg@coinbase.com>
2025-07-17 20:48:08 -0700

    Update foundry settings and deploy

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -7,7 +7,7 @@ import {WETHUnwrapper} from "../src/WETHUnwrapper.sol";
 contract Deploy is Script {
     address wethAddress = 0x4200000000000000000000000000000000000006;
 
-    bytes32 salt = keccak256("WETHUnwrapper");
+    bytes32 salt = 0x0000000000000000000000000000000000000000000000000000000000000000;
 
     function setUp() public {}
 
